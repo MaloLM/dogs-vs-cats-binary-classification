6717feec3e58effee0cccb0ae088c73e0e25633c
Malo LM <58141930+MaloLM@users.noreply.github.com>
2024-11-15 22:13:15 +0100

    new test accuracy reccord

--- code_stacking.py
+++ code_stacking.py
@@ -3,6 +3,151 @@
 #######
 
 
+#===================================================
+
+class CNNBinaryClassifier(nn.Module):
+    def __init__(self, input_shape=(3, 128, 128)):
+        super(CNNBinaryClassifier, self).__init__()
+        
+        # Convolutional Block 1
+        self.conv1_1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, padding=1)
+        self.conv1_2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1)
+        self.bnorm1 = nn.BatchNorm2d(64) 
+
+        # Convolutional Block 3
+        self.conv3_1 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1)
+        self.conv3_2 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1)
+        self.bnorm3 = nn.BatchNorm2d(128)
+        self.pool22 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # Convolutional Block 4 (with an additional convolutional layer)
+        self.conv4_1 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1)
+        self.conv4_2 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1)
+        self.bnorm4 = nn.BatchNorm2d(256)
+
+        # Global Average Pooling
+        self.global_avg_pool = nn.AdaptiveAvgPool2d((1, 1))  # Output (1, 1) spatially for each channel
+
+        # Fully Connected Layers
+        self.fc1 = nn.Linear(256, 64)
+        self.fc2 = nn.Linear(64, 1)
+
+        # Dropout
+        self.dropout = nn.Dropout(0.5)
+
+    def forward(self, x):
+        # Block 1
+        x = F.relu(self.conv1_1(x))
+        x = F.relu(self.conv1_2(x))
+        x = self.bnorm1(x)
+        x = self.pool22(x)
+
+        # Block 3
+        x = F.relu(self.conv3_1(x))
+        x = F.relu(self.conv3_2(x))
+        x = self.bnorm3(x)
+        x = self.pool22(x)
+
+        # Block 4
+        x = F.relu(self.conv4_1(x))
+        x = F.relu(self.conv4_2(x))
+        x = self.bnorm4(x)
+        x = self.pool22(x)
+
+        # Global Average Pooling
+        x = self.global_avg_pool(x)
+        x = x.view(-1, 256)  # Flatten for fully connected layers
+
+        # Fully connected layers with dropout
+        x = F.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = torch.sigmoid(self.fc2(x))  # Binary classification output
+
+        return x
+
+summary(CNNBinaryClassifier().cuda(), input_size = [(3, 128, 128)])
+
+#===================================================
+
+class CNNBinaryClassifier(nn.Module):
+    def __init__(self, input_shape=(3, 128, 128)):
+        super(CNNBinaryClassifier, self).__init__()
+        
+        # Convolutional Block 1
+        self.conv1_1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, padding=1)
+        self.conv1_2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, padding=1)
+        self.bnorm1 = nn.BatchNorm2d(32)  # BatchNorm after last conv in block
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)  # Downsampling by 2
+
+        # Convolutional Block 2
+        self.conv2_1 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
+        self.conv2_2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1)
+        self.bnorm2 = nn.BatchNorm2d(64)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # Convolutional Block 3
+        self.conv3_1 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1)
+        self.conv3_2 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1)
+        self.bnorm3 = nn.BatchNorm2d(128)
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # Convolutional Block 4 (with an additional convolutional layer)
+        self.conv4_1 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1)
+        self.conv4_2 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1)
+        self.conv4_3 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1)  # Extra layer
+        self.bnorm4 = nn.BatchNorm2d(256)
+        self.pool4 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # Global Average Pooling
+        self.global_avg_pool = nn.AdaptiveAvgPool2d((1, 1))  # Output (1, 1) spatially for each channel
+
+        # Fully Connected Layers
+        self.fc1 = nn.Linear(256, 64)
+        self.fc2 = nn.Linear(64, 1)
+
+        # Dropout
+        self.dropout = nn.Dropout(0.5)
+
+    def forward(self, x):
+        # Block 1
+        x = F.relu(self.conv1_1(x))
+        x = F.relu(self.conv1_2(x))
+        x = self.bnorm1(x)
+        x = self.pool1(x)
+
+        # Block 2
+        x = F.relu(self.conv2_1(x))
+        x = F.relu(self.conv2_2(x))
+        x = self.bnorm2(x)
+        x = self.pool2(x)
+
+        # Block 3
+        x = F.relu(self.conv3_1(x))
+        x = F.relu(self.conv3_2(x))
+        x = self.bnorm3(x)
+        x = self.pool3(x)
+
+        # Block 4
+        x = F.relu(self.conv4_1(x))
+        x = F.relu(self.conv4_2(x))
+        x = F.relu(self.conv4_3(x))  # Additional layer
+        x = self.bnorm4(x)
+        x = self.pool4(x)
+
+        # Global Average Pooling
+        x = self.global_avg_pool(x)
+        x = x.view(-1, 256)  # Flatten for fully connected layers
+
+        # Fully connected layers with dropout
+        x = F.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = torch.sigmoid(self.fc2(x))  # Binary classification output
+
+        return x
+
+summary(CNNBinaryClassifier().cuda(), input_size = [(3, 128, 128)])
+
+#===================================================
 
 predictions = []
 true_labels = []
